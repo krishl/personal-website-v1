2cf346d68fa807124953f67301fb7c7a60f6dc2c
github username <krishel.lasam@gmail.com>
2017-10-22 20:56:55 -0400

    space out post

--- _posts/2017-10-22-the_beginning_of_my_wordpress_journey.php
+++ _posts/2017-10-22-the_beginning_of_my_wordpress_journey.php
@@ -5,6 +5,8 @@ date:       2017-10-22 8:55:25 -0400
 permalink:  the_beginning_of_my_wordpress_journey
 ---
 
-This week for my eCommerce project, I switched the webhost over to Siteground and also switched platforms over to Woocommerce on Wordpress. I am mainly excited about the flexibility that Woocommerce provides, which is a huge step up from the previous eCommerce platform! It took about two days for the domain nameservers to completely propagate. I was also successful in activating the SSL certificate, which is a must for eCommerce. The initial platform set up was quick, and now I am beginning to learn the art of Wordpress. This will be a great opportunity for me to learn PHP, a language that I am just now beginning to learn. It seems to have many similarities to what I've learned throughout my time at Flatiron, so it doesn't seem like a daunting task at all.
+This week for my eCommerce project, I switched the webhost over to Siteground and also switched platforms over to Woocommerce on Wordpress.
 
-I'm excited for the events to come!
+I am mainly excited about the flexibility that Woocommerce provides, which is a huge step up from the previous eCommerce platform! It took about two days for the domain nameservers to completely propagate. I was also successful in activating the SSL certificate, which is a must for eCommerce. The initial platform set up was quick, and now I am beginning to learn the art of Wordpress. This will be a great opportunity for me to learn PHP, a language that I am just now beginning to learn. It seems to have many similarities to what I've learned throughout my time at Flatiron, so it doesn't seem like a daunting task at all.
+
+I'm eager for the events to come!
